84b699f8e16d25184f45c3798446ddf2914d9ab9
anvox <thien.an.vo.nguyen@gmail.com>
2024-07-05 13:55:14 +0700

    Update key

--- init.lua
+++ init.lua
@@ -285,8 +285,8 @@ require("lazy").setup({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<ESC>"] = cmp.mapping.abort(),
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
           { name = "copilot", group_index = 2 },
