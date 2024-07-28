853b3224f110e5493b498917e2b433206d69a5e8
anvox <thien.an.vo.nguyen@gmail.com>
2024-07-28 13:43:34 +0700

    Fix missing save

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -37,36 +37,4 @@ return {
       vim.keymap.set('n', '<leader>b', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
     end
   },
-  {
-    "navarasu/onedark.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      require('onedark').setup {
-        style = 'warmer'
-      }
-      require("onedark").load()
-    end,
-  },
-  {
-    'ahmedkhalf/project.nvim',
-    config = function()
-      require("project_nvim").setup({
-        detection_methods = { "pattern", "lsp" }
-      })
-    end,
-  },
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    }
-  },
 }
