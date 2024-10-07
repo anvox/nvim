b442034c13df3532ac739228d314c48edd315ad2
anvox <thien.an.vo.nguyen@gmail.com>
2024-10-07 11:30:02 +0700

    Add copilot chat

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -12,4 +12,18 @@ return {
     end
   },
   "jose-elias-alvarez/null-ls.nvim",
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },  -- for curl, log wrapper
+    },
+    build = "make tiktoken",        -- Only on MacOS or Linux
+    opts = {
+      debug = true,                 -- Enable debugging
+      -- See Configuration section for rest
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 }
