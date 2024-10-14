80397aa31bc6e6dc94bcb55c30f0fa3493547db1
anvox <thien.an.vo.nguyen@gmail.com>
2024-10-14 22:10:19 +0700

    Find files case insensitive

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,14 +12,57 @@ return {
         end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+      {
+        'nvim-tree/nvim-web-devicons',
+        enabled = vim.g.have_nerd_font
+      },
       { 'mollerhoj/telescope-recent-files.nvim' }
     },
     config = function()
       require('telescope').setup {
+        defaults = {
+          file_ignore_patterns = {},
+          file_sorter = require('telescope.sorters').get_fuzzy_file,
+          generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
+          },
+          ['recent-files'] = {
+            -- Add these settings for recent-files
+            only_cwd = true,
+            sorting = "mru",
+            case_mode = "ignore_case",
+            use_file_sorter = true,
+          },
+        },
+        pickers = {
+          find_files = {
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*", "-L" },
+            case_mode = "ignore_case",
+            on_input_filter_cb = function(prompt)
+              local file, line = prompt:match("(.+):(%d+)$")
+              if file and line then
+                return { prompt = file }
+              end
+            end,
+            attach_mappings = function(prompt_bufnr, map)
+              local actions = require('telescope.actions')
+              local action_state = require('telescope.actions.state')
+              actions.select_default:replace(function()
+                local prompt = action_state.get_current_line()
+                local file, line = prompt:match("(.+):(%d+)$")
+                actions.close(prompt_bufnr)
+                if file and line then
+                  vim.cmd('edit ' .. file)
+                  vim.api.nvim_win_set_cursor(0, { tonumber(line), 0 })
+                else
+                  actions.select_default()
+                end
+              end)
+              return true
+            end,
           },
         },
       }
@@ -32,7 +75,6 @@ return {
       local builtin = require 'telescope.builtin'
 
       vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
           previewer = false,
@@ -45,35 +87,16 @@ return {
           prompt_title = 'Live Grep in Open Files',
         }
       end, { desc = '[S]earch [/] in Open Files' })
-      vim.keymap.set('n', '<C-F>', require('telescope.builtin').live_grep, { desc = 'Search by Grep' })
+
+      vim.keymap.set('n', '<C-F>', function()
+        builtin.live_grep({ default_text = vim.fn.expand("<cword>") })
+      end, { desc = 'Search by Grep' })
 
       vim.keymap.set('n', '<leader><leader>', function()
-        require('telescope').extensions['recent-files'].recent_files({})
+        require('telescope').extensions['recent-files'].recent_files({
+          case_mode = "ignore_case",
+        })
       end, { noremap = true, silent = true })
     end,
-    pickers = {
-      find_files = {
-        on_input_filter_cb = function(prompt)
-          local file, line = prompt:match("(.+):(%d+)$")
-          if file and line then
-            return { prompt = file }
-          end
-        end,
-        attach_mappings = function(prompt_bufnr, map)
-          actions.select_default:replace(function()
-            local prompt = action_state.get_current_line()
-            local file, line = prompt:match("(.+):(%d+)$")
-            actions.close(prompt_bufnr)
-            if file and line then
-              vim.cmd('edit ' .. file)
-              vim.api.nvim_win_set_cursor(0, { tonumber(line), 0 })
-            else
-              actions.select_default()
-            end
-          end)
-          return true
-        end,
-      },
-    },
   },
 }
