594f2ba988dd1ad7224ef9747fc4bd68898cc347
anvox <thien.an.vo.nguyen@gmail.com>
2025-04-21 00:42:11 +0700

    Replace barbar with bufferline as it works with current onedark theme

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,7 +1,37 @@
 return {
+  -- {
+  --   "romgrk/barbar.nvim",
+  --   opts = { animation = false },
+  --   config = function()
+  --     local function buffer_close()
+  --       if vim.bo.modified then
+  --         local choice = vim.fn.confirm("Save changes?", "&Yes\n&No\n&Cancel")
+  --         if choice == 1 then
+  --           vim.cmd('write')
+  --         elseif choice == 2 then
+  --           vim.cmd('set nomodified')
+  --         else
+  --           return
+  --         end
+  --       end
+  --
+  --       if #vim.fn.getbufinfo({ buflisted = 1 }) > 1 then
+  --         vim.cmd('BufferClose')
+  --       else
+  --         vim.cmd('BufferClose')
+  --         vim.cmd('enew')
+  --         vim.cmd('setlocal bufhidden=wipe')
+  --       end
+  --     end
+  --     vim.keymap.set('n', '<leader>w', buffer_close, { noremap = true, silent = true })
+  --     vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferNext<CR>', { noremap = true, silent = true })
+  --     vim.keymap.set('n', '<leader>t', ':enew<CR>', { noremap = true, silent = true })
+  --   end
+  -- },
   {
-    "romgrk/barbar.nvim",
-    opts = { animation = true },
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
       local function buffer_close()
         if vim.bo.modified then
@@ -16,16 +46,18 @@ return {
         end
 
         if #vim.fn.getbufinfo({ buflisted = 1 }) > 1 then
-          vim.cmd('BufferClose')
+          vim.cmd(':bd')
         else
-          vim.cmd('BufferClose')
+          vim.cmd(':bd')
           vim.cmd('enew')
           vim.cmd('setlocal bufhidden=wipe')
         end
       end
       vim.keymap.set('n', '<leader>w', buffer_close, { noremap = true, silent = true })
-      vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferNext<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
       vim.keymap.set('n', '<leader>t', ':enew<CR>', { noremap = true, silent = true })
+
+      require("bufferline").setup({})
     end
-  },
+  }
 }
--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -6,7 +6,7 @@ return {
     opts = {},
     config = function()
       require('onedark').setup {
-        style = 'darker',
+        style = 'dark',
         code_style = {
           comments = 'italic',
           keywords = 'none',
@@ -31,4 +31,19 @@ return {
       })
     end,
   },
+  -- {
+  --   'projekt0n/github-nvim-theme',
+  --   name = 'github-theme',
+  --   lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   config = function()
+  --     require('github-theme').setup({
+  --       options = {
+  --         transparent = false
+  --       }
+  --     })
+  --
+  --     vim.cmd('colorscheme github_dark_default')
+  --   end,
+  -- }
 }
