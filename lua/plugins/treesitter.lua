548737a6fd4b74151cff5856c0acda704ab50093
anvox <thien.an.vo.nguyen@gmail.com>
2025-06-12 10:36:51 +0700

    Add terraform

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', "elixir", "eex", "heex", "javascript", "typescript" },
+      ensure_installed = { 'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', "elixir", "eex", "heex", "javascript", "typescript", "terraform", "hcl" },
       auto_install = true,
       highlight = {
         enable = true,
