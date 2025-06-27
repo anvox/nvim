bff0d235f036dafb882e58de57dcec2766db756c
anvox <thien.an.vo.nguyen@gmail.com>
2025-06-27 10:51:13 +0700

    Navigate between tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -24,11 +24,35 @@ return {
           vim.cmd('setlocal bufhidden=wipe')
         end
       end
-      vim.keymap.set('n', '<leader>w', buffer_close, { noremap = true, silent = true })
-      vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
-      vim.keymap.set('n', '<leader>t', ':enew<CR>', { noremap = true, silent = true })
+
+      -- 🗂️ Tab management keymaps
+      vim.keymap.set('n', '<leader>w', buffer_close, { noremap = true, silent = true, desc = '🗂️ Close buffer' })
+      vim.keymap.set('n', '<leader>t', ':enew<CR>', { noremap = true, silent = true, desc = '🗂️ New buffer' })
+
+      -- 🔄 Cycle through tabs
+      vim.keymap.set('n', '<C-h>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true, desc = '🔄 Previous tab' })
+      vim.keymap.set('n', '<C-l>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true, desc = '🔄 Next tab' })
+
+      -- 📌 Pin/unpin current tab
+      vim.keymap.set('n', '<C-p>', ':BufferLineTogglePin<CR>',
+        { noremap = true, silent = true, desc = '📌 Toggle pin tab' })
+
+      -- ⬅️➡️ Move current tab forward/backward
+      vim.keymap.set('n', '<C-g>', ':BufferLineMovePrev<CR>',
+        { noremap = true, silent = true, desc = '⬅️ Move tab left' })
+      vim.keymap.set('n', '<C-;>', ':BufferLineMoveNext<CR>',
+        { noremap = true, silent = true, desc = '➡️ Move tab right' })
 
       require("bufferline").setup({
+        options = {
+          -- 📌 Enable close icons for pinned buffers
+          show_close_icon = true,
+          show_buffer_close_icons = true,
+          -- 🎯 Better tab picking with numbers
+          numbers = "ordinal",
+          -- 📌 Show pin indicator
+          show_tab_indicators = true,
+        },
         highlights = {
           background = {
             fg = '#909090', -- inactive buffer text
@@ -41,6 +65,13 @@ return {
           },
           close_button_visible = {
             fg = '#c8c8c8',
+          },
+          -- 📌 Pin indicator styling
+          indicator_visible = {
+            fg = '#ffd700', -- gold color for pin indicator
+          },
+          indicator_selected = {
+            fg = '#ffd700',
           },
         },
       })
