2a0c128e397647a6356e8862acc92978ee03964a
anvox <thien.an.vo.nguyen@gmail.com>
2026-06-17 10:49:10 +0700

    Use romus204/tree-sitter-manager for neovim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,26 @@
--- return {
---   {
---     'nvim-treesitter/nvim-treesitter',
---     build = ':TSUpdate',
---     opts = {
---       ensure_installed = { 'bash', 'diff', 'html', 'lua', 'luadoc', 'markdown', "elixir", "eex", "heex", "javascript", "typescript", "terraform", "hcl" },
---       auto_install = true,
---       highlight = {
---         enable = true,
---         additional_vim_regex_highlighting = { 'ruby' },
---       },
---       indent = { enable = true, disable = { 'ruby' } },
---     },
---     config = function(_, opts)
---       require('nvim-treesitter.install').prefer_git = true
---       ---@diagnostic disable-next-line: missing-fields
---       require('nvim-treesitter.configs').setup(opts)
---     end,
---   },
--- }
+-- 🤖 brew install tree-sitter
+return {
+  {
+    "romus204/tree-sitter-manager.nvim",
+    config = function()
+      require("tree-sitter-manager").setup({
+        auto_install = true,
+        ensure_installed = {
+          "bash",
+          "diff",
+          "html",
+          "lua",
+          "luadoc",
+          "markdown",
+          "elixir",
+          "eex",
+          "heex",
+          "javascript",
+          "typescript",
+          "terraform",
+          "hcl",
+        },
+      })
+    end,
+  },
+}
